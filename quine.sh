f631f826948f7faf3b13fdf8b858dc0a82b2101c
Ivan Molella <>
2022-12-23 11:37:21 +0100

    quine, simple C self reproducing program

--- quine.sh
+++ quine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat <<EOF > quine.c && gcc ./quine.c -o quine && ./quine > thesame.c && DIFF=$(diff quine.c thesame.c) && [ "$DIFF" == "" ] && echo "Source are identical"
+cat <<EOF > quine.c && gcc ./quine.c -o quine && ./quine > thesame.c && DIFF=$(diff quine.c thesame.c) && [ "$DIFF" == "" ] && echo "Sources are identical"
 #include <stdio.h>
 char* _ = "#include <stdio.h>%cchar* _ = %c%s%c;%cint main(){printf(_,10,34,_,34,10,10);}%c";
 int main(){printf(_,10,34,_,34,10,10);}
